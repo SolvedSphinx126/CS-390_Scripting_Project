8151c023deaec2419fc639b417cf93d469461816
Ben O'Bryan <obryanbw@gmail.com>
2021-11-18 00:16:41 -0600

    Finished?

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import requests
 import re
 
-
-def print_hi(name):
-    print("Hi, {0}".format(name))
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    lastName = "Zhang"
-    linkPattern = "(http://www.uah.edu/cgi-bin/schedule.pl\?file=[^\.]*\.html&segment=NDX)"
-    namePattern = ".*" + lastName + ".*"
+    lastName = "Menon"
     web = requests.get("https://www.uah.edu/cgi-bin/schedule.pl")
-    links = re.findall(linkPattern, web.content.decode())
-    classes = []
+    links = re.findall("(http://www.uah.edu/cgi-bin/schedule.pl\?file=[^\.]*\.html&segment=NDX)", web.content.decode())
+    rawClasses = []
+    semesters = []
     for i in range(len(links)):
         links[i] = links[i].replace(links[i][-3:], "CS")
-        classes.append(re.findall(namePattern, requests.get(links[i]).content.decode()))
-    for clas in classes:
-        print(str(clas))
+        rawClasses.append(re.findall(".*" + lastName + ".*", requests.get(links[i]).content.decode()))
+    for clas in rawClasses:
+        semesters.append(re.findall("((\S+ )+\S+(?= +((\d\.\d)|(\.\d))))", str(clas).replace(',', '\n')))
+
+    for semester in semesters:
+        for session in semester:
+            print(session[0])
