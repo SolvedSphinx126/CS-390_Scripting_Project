bc0f842eb8f2177d84843b85a76ef4fbc4ebcb8e
SolvedSphinx126 <bwo0002@gmail.com>
2021-11-17 22:33:21 -0600

    update

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    pattern = "(http://www.uah.edu/cgi-bin/schedule.pl\?file=[^\.]*\.html&segment=NDX)"
+    lastName = "Zhang"
+    linkPattern = "(http://www.uah.edu/cgi-bin/schedule.pl\?file=[^\.]*\.html&segment=NDX)"
+    namePattern = ".*" + lastName + ".*"
     web = requests.get("https://www.uah.edu/cgi-bin/schedule.pl")
-    links = re.findall(pattern, web.content.decode())
-    for link in links:
-        link = link.replace(link[-3:],"CS")
-    print(links)
+    links = re.findall(linkPattern, web.content.decode())
+    classes = []
+    for i in range(len(links)):
+        links[i] = links[i].replace(links[i][-3:], "CS")
+        classes.append(re.findall(namePattern, requests.get(links[i]).content.decode()))
+    for clas in classes:
+        print(str(clas))
