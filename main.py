1b3b64ddc1210476b869ab2242ade60f494f4b3f
Ben O'Bryan <obryanbw@gmail.com>
2021-11-20 11:12:57 -0600

    Finished?

--- main.py
+++ main.py
@@ -2,17 +2,11 @@ import requests
 import re
 
 if __name__ == '__main__':
-    lastName = "Menon"
+    lastName = input("Enter instructor last name: ")
     web = requests.get("https://www.uah.edu/cgi-bin/schedule.pl")
     links = re.findall("(http://www.uah.edu/cgi-bin/schedule.pl\?file=[^\.]*\.html&segment=NDX)", web.content.decode())
-    rawClasses = []
-    semesters = []
     for i in range(len(links)):
         links[i] = links[i].replace(links[i][-3:], "CS")
-        rawClasses.append(re.findall(".*" + lastName + ".*", requests.get(links[i]).content.decode()))
-    for clas in rawClasses:
-        semesters.append(re.findall("((\S+ )+\S+(?= +((\d\.\d)|(\.\d))))", str(clas).replace(',', '\n')))
-
-    for semester in semesters:
-        for session in semester:
-            print(session[0])
+        print(links[i][44:52])
+        for session in re.findall(".*" + lastName + ".*", requests.get(links[i]).content.decode()):
+            print(session)
