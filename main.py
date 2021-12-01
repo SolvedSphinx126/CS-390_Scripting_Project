d46f559eca90a58ff446a0916aad73be7bfbbf0e
Ben O'Bryan <50787535+SolvedSphinx126@users.noreply.github.com>
2021-11-30 22:17:38 -0600

    comments

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import requests
-import re
+import requests #import the requests package
+import re       #import the regex package
 
 if __name__ == '__main__':
-    lastName = input("Enter instructor last name: ")
-    web = requests.get("https://www.uah.edu/cgi-bin/schedule.pl")
-    links = re.findall("(http://www.uah.edu/cgi-bin/schedule.pl\?file=[^\.]*\.html&segment=NDX)", web.content.decode())
+    lastName = input("Enter instructor last name: ")                #prompt for a teacher's last name (ex. Zhang)
+    web = requests.get("https://www.uah.edu/cgi-bin/schedule.pl")   #get content of the page that lists each of the semesters that need to be checked
+    links = re.findall("(http://www.uah.edu/cgi-bin/schedule.pl\?file=[^\.]*\.html&segment=NDX)", web.content.decode()) #get the list of those valid links
     for i in range(len(links)):
-        links[i] = links[i].replace(links[i][-3:], "CS")
-        print(links[i][44:52])
-        for session in re.findall(".*" + lastName + ".*", requests.get(links[i]).content.decode()):
-            print(session)
+        links[i] = links[i].replace(links[i][-3:], "CS")    #for each of the links, select the CS dept
+        print(links[i][44:52])                              #label each semester
+        for session in re.findall(".*" + lastName + ".*", requests.get(links[i]).content.decode()): #select each class that is taught by the requested teacher
+            print(session)                                                                          #and print it
